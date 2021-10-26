5cacdb4aee61b00b351f81696baf69f2ee47801c
Houcem Hedhly <hedhoucem@gmail.com>
2021-10-26 16:39:11 +0100

    scenario 2 - method 2

--- ult2021-groupa-courses/src/app/app.module.ts
+++ ult2021-groupa-courses/src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from "@angular/router";
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 
+const routes: Routes = [];
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+ }
