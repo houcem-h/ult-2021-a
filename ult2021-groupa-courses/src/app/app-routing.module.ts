9fd707f53de4b007630109cb3df7c78438259bcf
Houcem Hedhly <hedhoucem@gmail.com>
2021-11-02 15:02:16 +0100

    not found component

--- ult2021-groupa-courses/src/app/app-routing.module.ts
+++ ult2021-groupa-courses/src/app/app-routing.module.ts
@@ -3,18 +3,21 @@ import { RouterModule, Routes } from "@angular/router";
 import { AboutComponent } from "./about/about.component";
 import { ContactComponent } from "./contact/contact.component";
 import { HomeComponent } from "./home/home.component";
+import { NotFoundComponent } from './not-found/not-found.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'contact', component: ContactComponent },
+  { path:'**', component: NotFoundComponent }
 ];
 
 @NgModule({
   declarations: [
     AboutComponent,
     ContactComponent,
-    HomeComponent
+    HomeComponent,
+    NotFoundComponent
   ],
   imports: [
     RouterModule.forRoot(routes)
