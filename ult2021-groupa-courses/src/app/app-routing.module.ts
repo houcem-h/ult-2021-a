e29db7b7a848e93fcef8f653a303da65a92207fc
Houcem Hedhly <hedhoucem@gmail.com>
2021-11-23 14:56:58 +0100

    two-way binding example

--- ult2021-groupa-courses/src/app/app-routing.module.ts
+++ ult2021-groupa-courses/src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { AboutComponent } from "./about/about.component";
 import { ContactComponent } from "./contact/contact.component";
 import { HomeComponent } from "./home/home.component";
 import { NotFoundComponent } from './not-found/not-found.component';
+import { FormsModule } from "@angular/forms";
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
@@ -21,7 +22,7 @@ const routes: Routes = [
     NotFoundComponent
   ],
   imports: [
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes), FormsModule
   ],
   exports: [
     RouterModule
