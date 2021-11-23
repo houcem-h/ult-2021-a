da90f7f092d130d7d9257afd7e82a772a8f238b0
Houcem Hedhly <hedhoucem@gmail.com>
2021-11-23 14:32:37 +0100

    string interpolation example

--- ult2021-groupa-courses/src/app/about/about.component.ts
+++ ult2021-groupa-courses/src/app/about/about.component.ts
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
+  public user1 = {
+    name: "John Doe",
+    age: "25",
+    email: "john.doe@gmail.com"
+  }
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  public reverseName() {
+    return this.user1.name.split('').reverse().join('');
   }
 
 }
