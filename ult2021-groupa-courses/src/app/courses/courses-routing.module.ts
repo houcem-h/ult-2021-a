77a848499c68deddea361ab144fc242aabf466cc
Houcem Hedhly <hedhoucem@gmail.com>
2021-12-07 14:54:32 +0100

    disaply course details

--- ult2021-groupa-courses/src/app/courses/courses-routing.module.ts
+++ ult2021-groupa-courses/src/app/courses/courses-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CommonModule } from "@angular/common";
+
 import { CoursesComponent } from './courses.component';
 import { CourseDetailsComponent } from './course-details/course-details.component';
 
@@ -9,7 +11,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
+  imports: [RouterModule.forChild(routes), CommonModule],
   exports: [RouterModule],
   declarations: [
     CourseDetailsComponent
