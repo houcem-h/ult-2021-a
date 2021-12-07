39c95d4746d4d98e4f7622c6b0eda3598da97be1
Houcem Hedhly <hedhoucem@gmail.com>
2021-12-07 14:33:02 +0100

    ng g c courses/course-details -m courses-routing

--- ult2021-groupa-courses/src/app/courses/courses-routing.module.ts
+++ ult2021-groupa-courses/src/app/courses/courses-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CoursesComponent } from './courses.component';
+import { CourseDetailsComponent } from './course-details/course-details.component';
 
 const routes: Routes = [{ path: '', component: CoursesComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  declarations: [
+    CourseDetailsComponent
+  ]
 })
 export class CoursesRoutingModule { }
