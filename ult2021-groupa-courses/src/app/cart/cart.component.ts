63df62a863183ca0284d171a623fa2f8e8a84e10
Houcem Hedhly <hedhoucem@gmail.com>
2021-12-07 16:48:36 +0100

    compute and display total price

--- ult2021-groupa-courses/src/app/cart/cart.component.ts
+++ ult2021-groupa-courses/src/app/cart/cart.component.ts
@@ -11,21 +11,30 @@ import { courses } from "src/app/courses-list";
 export class CartComponent implements OnInit {
 
   public cartContent: any = [];
+  totalPrice: number = 0;
+  TAX_RATE: number = 0.2;
 
   constructor(
     private cartService: CartService
   ) { }
 
   ngOnInit(): void {
-    this.cartContent = this.cartService.cartContent;
+    this.getCartContentDetails();
+    this.computeTotalPrice();
+  }
 
+  getCartContentDetails() {
+    this.cartContent = this.cartService.cartContent;
     for (let index = 0; index < this.cartContent.length; index++) {
       const course = courses.filter(course => course.id == this.cartContent[index].id)[0];
       this.cartContent[index].title = course.title;
       this.cartContent[index].price = course.price;
     }
-    console.log(this.cartContent);
-
+  }
+  computeTotalPrice() {
+    this.cartContent.forEach((item: { price: number; quantity: number; }) => {
+      this.totalPrice += item.price * item.quantity;
+    });
   }
 
 }
