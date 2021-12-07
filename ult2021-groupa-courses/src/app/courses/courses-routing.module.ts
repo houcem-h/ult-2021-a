6450b3dfed878a6c2d67010f08c014b8910e93e2
Houcem Hedhly <hedhoucem@gmail.com>
2021-12-07 14:39:14 +0100

    create a routerLink for course details component

--- ult2021-groupa-courses/src/app/courses/courses-routing.module.ts
+++ ult2021-groupa-courses/src/app/courses/courses-routing.module.ts
@@ -3,7 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { CoursesComponent } from './courses.component';
 import { CourseDetailsComponent } from './course-details/course-details.component';
 
-const routes: Routes = [{ path: '', component: CoursesComponent }];
+const routes: Routes = [
+  { path: '', component: CoursesComponent },
+  { path: ':id', component: CourseDetailsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
