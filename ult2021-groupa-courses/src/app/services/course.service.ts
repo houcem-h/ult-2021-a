eb214d460e30bd1a5d0e1ee8107fbc7503572e9e
Houcem Hedhly <hedhoucem@gmail.com>
2021-12-08 10:54:31 +0100

    request backend api

--- ult2021-groupa-courses/src/app/services/course.service.ts
+++ ult2021-groupa-courses/src/app/services/course.service.ts
@@ -8,7 +8,8 @@ import { HttpClient } from "@angular/common/http";
 export class CourseService {
 
   // private apiUrl = 'http://localhost:3000/courses';
-  private apiUrl = 'https://my-json-server.typicode.com/houcem-h/public_courses_api/courses';
+  // private apiUrl = 'https://my-json-server.typicode.com/houcem-h/public_courses_api/courses';
+  private apiUrl = 'http://localhost:3000/api/courses';
 
   constructor(private http: HttpClient) { }
 
