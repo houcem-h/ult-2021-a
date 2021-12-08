8d080f6f7e6477a9ce4a1758fa6e419661c1deba
Houcem Hedhly <hedhoucem@gmail.com>
2021-12-08 10:43:28 +0100

    create a route in the app.js file

--- ult2021-groupa-courses/backend/app.js
+++ ult2021-groupa-courses/backend/app.js
@@ -2,4 +2,8 @@ const express = require('express');
 
 const app = express();
 
+app.use((req, res) => {
+  res.json('This is the new server response using Express.js !');
+});
+
 module.exports = app;
